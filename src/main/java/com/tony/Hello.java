3a0cd71972fc581ff9036d92d84839178f20055a
Tony <elsu6fu0@gmail.com>
2019-06-08 18:00:33 +0800

    第二次commit

---
+++ src/main/java/com/tony/Hello.java
@@ -0,0 +1,15 @@
+package com.tony;
+
+public class Hello {
+    public static void main(String[] args) {
+
+//        System.out.print("Hello World");
+
+      Person p = new Person();
+      p.hello();
+
+    }
+
+
+
+}
---
+++ src/main/java/com/tony/Person.java
@@ -0,0 +1,9 @@
+package com.tony;
+
+public class Person {
+
+    public void hello(){
+//        System.out.print("Hello World");
+    }
+
+}
